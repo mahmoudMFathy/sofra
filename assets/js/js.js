0635a01eddab7e23b232b02b961760b3b1096b52
mahmoudMFathy <50025676+mahmoudMFathy@users.noreply.github.com>
2020-01-01 07:35:51 +0200

    1/1/2019commit

--- assets/js/js.js
+++ assets/js/js.js
@@ -20,6 +20,49 @@ $(document).ready(function () {
     // 
 
 
+    $('.responsivemorefood').slick({
+        dots: true,
+        rtl: true,
+        // infinite: true,
+        // speed: 300,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
     $('.responsive').slick({
         dots: true,
         infinite: false,
